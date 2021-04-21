4edd71703f2cfdcef1db067c7b5ccccd8ba23211
josh <josh@thambi.li>
2021-04-22 09:08:32 +1000

    Fix csvs

--- bclscrape/test.py
+++ bclscrape/test.py
@@ -7,23 +7,63 @@ sheets_file = here + "/balance_sheets/"
 summaries_file = here + "/statistical_summaries/"
 summary_csvs = here + "/summary_csvs/"
 
-ceevee = f"{summary_csvs}1990summary.csv"
+metrics = ['Ore and waste removed (millions of tonnes)', 'Total concentrate (thousands of dry tonnes)', 'Net earnings (loss)', 'Local employees']
 
-df = pd.read_csv(ceevee, header=None)
-df.columns= ['unamed', 'None', '1990', '1989', '1988', '1987', '1986', '1985']
+listo = []
 
-# df = df[['unamed', '1990', '1989', '1988', '1987', '1986', '1985']]
+# for csv in os.listdir(summary_csvs):
+#     path = f"{summary_csvs}{csv}"
+#     df = pd.read_csv(path)
+#     df.rename(columns={df.columns[0]: "Metric"}, inplace=True)
+#     # df['Metric'] = df['Metric'].fillna(0)
+#     df.loc[df['Metric'].str.contains('Ore and waste').fillna(False), 'Metric'] = 'Ore and waste removed (millions of tonnes)'
+#     df.loc[df['Metric'].str.contains('Total concentrate').fillna(False), 'Metric'] = 'Total concentrate (thousands of dry tonnes)'
+#     df.loc[df['Metric'].str.contains('Net earnings').fillna(False), 'Metric'] = 'Net earnings (loss)'
+#     df.loc[df['Metric'].str.contains('National').fillna(False), 'Metric'] = 'Local employees'
 
-for year in ['1990', '1989', '1988', '1987', '1986', '1985']:
-    df[year] = df[year].astype(str)
-    df[year] = df[year].str.replace("B", '8')
-    df[year] = df[year].str.replace('"', '')
+#     df = df.loc[df['Metric'].isin(metrics)]
+#     print(df)
+#     listo.append(df)
+# # 'Ore and waste'
 
-    # df[year] = pd.to_numeric(df[year])
+#     # df = df.iloc[:, 0].str.contains('Ore and waste')
+#     # print(df.loc[(df['Metric'].str.contains('Dividends').fillna(False)) | df['Metric'].str.contains('Dividends paid').fillna(False)])
 
-# df[['1990', '1989', '1988', '1987', '1986', '1985']] = df[['1990', '1989', '1988', '1987', '1986', '1985']].str.replace("B", '8')
 
+
+df = pd.read_csv('/Users/josh_nicholas/github/pacific_extractives_trade/bclscrape/summary_csvs/2008summary.csv')
+
+
+df["2007 2006"] = df["2007 2006"].fillna("0 0")
+# df[['2007', '2006']] = df["2007 2006"].str.split(' ', expand=True)
+
+df["2007 2006"] = df["2007 2006"].str.strip()
+
+df['2007']= df["2007 2006"].str.split(' ', expand=True)[0]
+df['2006']= df["2007 2006"].str.split(' ', expand=True)[1]
+
+# print(df["2007 2006"].fillna("0 0"))
 print(df)
 
-with open(f"{summary_csvs}1990summary.csv", "w") as f:
-    df.to_csv(f, index=False, header=True)
+### OLD CODE FOR RENAMING
+
+# ceevee = f"{summary_csvs}1990summary.csv"
+
+# df = pd.read_csv(ceevee, header=None)
+# df.columns= ['unamed', 'None', '1990', '1989', '1988', '1987', '1986', '1985']
+
+# # df = df[['unamed', '1990', '1989', '1988', '1987', '1986', '1985']]
+
+# for year in ['1990', '1989', '1988', '1987', '1986', '1985']:
+#     df[year] = df[year].astype(str)
+#     df[year] = df[year].str.replace("B", '8')
+#     df[year] = df[year].str.replace('"', '')
+
+#     # df[year] = pd.to_numeric(df[year])
+
+# # df[['1990', '1989', '1988', '1987', '1986', '1985']] = df[['1990', '1989', '1988', '1987', '1986', '1985']].str.replace("B", '8')
+
+# print(df)
+
+# with open(f"{summary_csvs}1990summary.csv", "w") as f:
+#     df.to_csv(f, index=False, header=True)
